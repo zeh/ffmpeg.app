a3263a54418f76c457285c26e810cebeee4aa177
Zeh Fernando <zeh@zehfernando.com>
2022-03-14 22:22:01 -0400

    Components: basic app component and styles

---
+++ src/components/App/index.tsx
@@ -0,0 +1,9 @@
+import classes from "./index.module.css";
+
+export function App(): JSX.Element {
+	return (
+		<div className={classes.container}>
+			<p>App</p>
+		</div>
+	);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { render } from "preact";
-import { App } from "./app";
+import { App } from "./components/App";
 import "./index.css";
 
-render(<App />, document.getElementById("app") as HTMLElement);
+render(<App />, document.getElementById("content") as HTMLElement);

f02f7dfde185a597820eb784b905980ef849f530
Zeh Fernando <zeh@zehfernando.com>
2022-03-21 22:17:39 -0400

    Chore: added postcss-mixins

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	plugins: [
+		require("postcss-mixins"),
 		require("autoprefixer"),
 		require("cssnano")({ preset: ["default", { discardComments: { removeAll: true } }] }),
 	],

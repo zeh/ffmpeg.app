a86c818203d51a4d17c156a7b29d249e0aa32548
Zeh Fernando <zeh@zehfernando.com>
2023-05-01 22:58:22 -0400

    Build: added cross-origin headers to Vite config
    This is needed so we can use SharedArrayBuffers locally, when testing.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()]
+	plugins: [
+		preact(),
+		// Fix cross-origin-isolation so we can use SharedArrayBuffers
+		// See: https://github.com/ffmpegwasm/ffmpeg.wasm/issues/106
+		// From: https://github.com/chaosprint/vite-plugin-cross-origin-isolation
+		{
+			name: "configure-response-headers",
+			configureServer: (server) => {
+				server.middlewares.use((_req, res, next) => {
+					res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+					res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+					next();
+				});
+			},
+		},
+	]
 });

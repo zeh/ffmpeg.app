63723bec240a9cfebd24d181fa27c36948429502
Zeh Fernando <zeh@zehfernando.com>
2023-07-31 16:32:45 -0400

    Components: button can be disabled, and have a progress indicator

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,13 +4,28 @@ import s from "./styles.module.css";
 
 interface IProps {
 	text: string;
+	disabled?: boolean;
+	progress?: number;
 	className?: string;
 	onClick?: () => void;
 }
 
-export const Button = ({ className, onClick, text }: IProps): JSX.Element => {
+export const Button = ({ className, disabled, onClick, progress, text }: IProps): JSX.Element => {
+	const f = progress ?? 1;
+	const enabled = !disabled;
+	const progressStyle =
+		progress === 1
+			? { display: "none" }
+			: {
+					left: f * 100 + "%",
+			  };
 	return (
-		<button className={cx([className, s.container])} onClick={onClick}>
+		<button
+			className={cx([className, s.container, enabled ? undefined : s.disabled])}
+			disabled={!enabled}
+			onClick={onClick}
+		>
+			<div className={s.backgroundProgress} style={progressStyle} />
 			{text}
 		</button>
 	);
